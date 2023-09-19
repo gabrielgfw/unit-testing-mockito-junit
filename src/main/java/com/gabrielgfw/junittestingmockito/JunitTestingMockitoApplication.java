6ee263f71788782622d74717305bdb486de82c0d
Gabriel Felipe Werner <gabriel_gfw@hotmail.com>
2023-09-18 21:04:03 -0300

    first commit with basic spring project

---
+++ src/main/java/com/gabrielgfw/junittestingmockito/JunitTestingMockitoApplication.java
@@ -0,0 +1,13 @@
+package com.gabrielgfw.junittestingmockito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunitTestingMockitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JunitTestingMockitoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gabrielgfw/junittestingmockito/JunitTestingMockitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrielgfw.junittestingmockito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunitTestingMockitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
